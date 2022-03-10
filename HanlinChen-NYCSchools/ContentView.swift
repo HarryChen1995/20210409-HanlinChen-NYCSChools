abd300e3affc52cc6ad7f2c4a53f080b6aafabf5
harrychen1995 <harrychen1995@yahoo.com>
2022-03-09 20:04:30 -0500

    Initial Commit

---
+++ HanlinChen-NYCSchools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HanlinChen-NYCSchools
+//
+//  Created by Hanlin Chen on 3/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HanlinChen-NYCSchools/HanlinChen_NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HanlinChen_NYCSchoolsApp.swift
+//  HanlinChen-NYCSchools
+//
+//  Created by Hanlin Chen on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HanlinChen_NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
